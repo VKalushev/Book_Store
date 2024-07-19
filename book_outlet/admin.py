e065e64fedbce78d4ab95c4a76e73bb9bfd712ec
VKalushev <vasko.kalushev@gmail.com>
2024-07-19 23:33:04 +0300

    finished

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book,Author,Address,Country
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {'slug': ("title",)}
+    list_filter = ("author","rating")
+    list_display = ("title", "author")
+
+class AuthorAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    list_filter = ("first_name","last_name")
+    list_display = ("first_name","last_name", "address")
+
+class AddressAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    list_filter = ("city","postal_code")
+    list_display = ("city","postal_code")
+
+class CountryAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    list_filter = ("name",)
+    list_display = ("name","code")
+
 
 admin.site.register(Book,BookAdmin)
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Address,AddressAdmin)
+admin.site.register(Country,CountryAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,17 +5,51 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.name} - {self.code}"
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.city} - {self.street}, {self.postal_code}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)],default=1)
-    author = models.CharField(max_length=100,default="UNKNOWN")
+        validators=[MinValueValidator(1), MaxValueValidator(5)], default=1)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_best_selling = models.BooleanField(default=False)
-    slug = models.SlugField(default="",null=False,db_index=True)
+    slug = models.SlugField(default="", null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country, related_name="books")
 
     def __str__(self):
         return f"{self.title} - Rating: {self.rating}, Written By: {self.author}"
-    
+
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
-    

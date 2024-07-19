3e9638cd3599a089d3f9a992cbf4947037cbc720
VKalushev <vasko.kalushev@gmail.com>
2024-07-19 16:32:31 +0300

    Adding the use of Admin

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -10,6 +11,11 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)],default=1)
     author = models.CharField(max_length=100,default="UNKNOWN")
     is_best_selling = models.BooleanField(default=False)
+    slug = models.SlugField(default="",null=False,db_index=True)
 
     def __str__(self):
         return f"{self.title} - Rating: {self.rating}, Written By: {self.author}"
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[self.slug])
+    

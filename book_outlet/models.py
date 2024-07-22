d0fac992aeb08b8d65bd7aae81b84bbfd9c97ea1
VKalushev <vasko.kalushev@gmail.com>
2024-07-22 16:25:24 +0300

    Update models.py

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -32,7 +32,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     address = models.OneToOneField(
-        Address, on_delete=models.CASCADE, null=True)
+        Address, on_delete=models.CASCADE, null=True) 
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
